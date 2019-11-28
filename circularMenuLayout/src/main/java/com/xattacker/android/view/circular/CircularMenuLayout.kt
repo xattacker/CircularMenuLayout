18e563ef20413ff66c3cc02e20b59b6626b7f268
xattacker <xattacker.tao@gmail.com>
2019-11-28 15:23:59 +0800

    separated API CircularMenuLayout to another Module

--- circularMenuLayout/src/main/java/com/xattacker/android/view/circular/CircularMenuLayout.kt
+++ circularMenuLayout/src/main/java/com/xattacker/android/view/circular/CircularMenuLayout.kt
@@ -11,7 +11,7 @@ import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.LinearInterpolator
-import com.xattacker.android.circularmenulayout.R
+import com.xattacker.android.view.circular.R
 import com.xattacker.util.WeakReferenceList
 import java.lang.ref.WeakReference
 
